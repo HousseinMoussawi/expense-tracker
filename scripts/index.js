db4f8cbd40fe346ed0ace48de1184eccf45fa02b
H-M-1999 <81930506@students.liu.edu.lb>
2024-03-10 23:58:24 +0200

    finished the transaction creation function

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,19 @@ const symbol = document.getElementById('symbol')
 const balance_currency = document.getElementById('balance-currency')
 const balance_amount = document.getElementById('balance-amount')
 const create = document.getElementById('create')
+const create_amount = document.getElementById('create-amount')
+const create_type = document.getElementById('create-type')
+const create_currency = document.getElementById('create-currency')
+
+
+let transaction = []
+let balance = 10000
+localStorage.setItem('balance', balance)
+
 
 
 const converter = (from, to, amount) => {
-    const convert = axios.post("https://crowded-cyan-wildebeest.cyclic.app/students/convert", {  
+    axios.post("https://crowded-cyan-wildebeest.cyclic.app/students/convert", {  
         "from": from,
         "to": to,
         "amount": amount
@@ -19,7 +28,7 @@ const converter = (from, to, amount) => {
 
 
 
-const currencies = axios.get("https://crowded-cyan-wildebeest.cyclic.app/students/available")
+const currencies = axios.get("https://rich-erin-angler-hem.cyclic.app/students/available")
     .then(response => {
         
         const addSymbol = () => {
@@ -38,4 +47,24 @@ const currencies = axios.get("https://crowded-cyan-wildebeest.cyclic.app/student
     }) 
 
 
+const createTransaction = (amount, type, currency,id) => {
+    localStorage.getItem('id') ? id = localStorage.getItem('id') : id = 0
+    transaction.push({
+        "id": id,
+        "amount": amount,
+        "type": type,
+        "currency": currency
+    })
+    id++
+    localStorage.setItem('id', id)
     
+    localStorage.setItem('transaction', JSON.stringify(transaction))
+}
+
+create.addEventListener('click', () => {
+    createTransaction(create_amount.value, create_type.value, create_currency.value)
+    if(create_type.value === 'INCOME'){
+        balance += parseInt(create_amount.value)
+    }
+    
+})
